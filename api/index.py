6c4c00b514ffdcf5345d7c7896070ef968ca21b9
Ash-the-k <ashlesh.kanchan@gmail.com>
2025-02-07 15:21:25 +0530

    verc

--- api/index.py
+++ api/index.py
@@ -33,4 +33,4 @@ def handle_response():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
+    app.run(host='0.0.0.0', port=8000, debug=True)
